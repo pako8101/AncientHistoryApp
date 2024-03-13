562b2f04426cb45e29f1b99e950db5509e1b9096
pako8101 <pako810129@gmail.com>
2024-03-13 13:21:29 +0200

    Initial commit

---
+++ src/main/java/AncientHistoryApp/AncientHistoryAppApplication.java
@@ -0,0 +1,13 @@
+package AncientHistoryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientHistoryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientHistoryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/AncientHistoryApp/AncientHistoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package AncientHistoryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncientHistoryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
